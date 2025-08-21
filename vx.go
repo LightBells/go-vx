f28d7e348afddb2dc8832a6a1e183cd0178462e5
Nautilus <takahasi0928@gmail.com>
2025-08-21 17:03:21 +0900

    Update vx.go

--- vx.go
+++ vx.go
@@ -12,6 +12,28 @@ import (
 	"unsafe"
 )
 
+func AlignedAlloc2D(dim int, size int) [][]float32 {
+	alloc_size := size*dim
+	alloc_size_ := alloc_size
+	size = align(alloc_size)
+	ptr := C.aligned_alloc((C.size_t)(C.sizeof_float * vectorLength()), (C.size_t)(C.sizeof_float*alloc_size))
+	hdr := reflect.SliceHeader{
+		Data: uintptr(unsafe.Pointer(ptr)),
+		Len:  alloc_size,
+		Cap:  alloc_size,
+	}
+	goSliceToFill := *(*[]float32)(unsafe.Pointer(&hdr))
+	if alloc_size_ != alloc_size {
+		for i := alloc_size_; i < size; i++ {
+			goSliceToFill[i] = 0.0
+		}
+	}
+
+	goSlice := *(*[][]float32)(unsafe.Pointer(&hdr))
+	return goSlice
+}
+
+
 func AlignedAlloc(size int) []float32 {
 	size_ := size
 	size = align(size)
